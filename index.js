f1403294cbea50c58ad31b8b19ee59acae3fc745
paulfedo <90898055+paulfedo@users.noreply.github.com>
2025-06-15 11:43:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>In Victor  $$$ App</h1> <h4>Message: Success Done Updating</h4> <p>Version 6.0.0</p>');
+  res.send('<h1>In ABCTom  $$$ App</h1> <h4>Message: Success Done Updating</h4> <p>Version 6.0.0</p>');
  })
 
 
